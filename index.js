69f6402524b3033fa97e7e78b1b0d4a0cf581e68
MarkTocino <Mark_tocino@yahoo.com>
2023-05-18 20:25:07 -0400

    Completed Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+    // Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield"
+];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+};
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+};
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+};
+function appendCat(name) {
+    const appendCat = [...cats]
+    appendCat.push("Broom")
+    return appendCat
+};
+function prependCat(name){
+    const prependCat = cats.slice(0)
+    prependCat.unshift ("Arnold");
+    return prependCat
+};
+function removeLastCat(name) {
+    const removeLastCat = cats.slice(0)
+    removeLastCat.pop()
+    return removeLastCat
+};
+function removeFirstCat() {
+    const removeFirstCat = [...cats]
+    removeFirstCat.shift()
+    return removeFirstCat
+};
